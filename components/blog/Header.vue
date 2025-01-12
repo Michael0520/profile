7b1eb971a4a3d8bd4747cafbc9baa2f7e48817e0
Michael0520 <michael860520@gmail.com>
2025-01-13 00:43:41 +0800

    style: refine Header component layout and styling
    - Adjusted header title alignment from center to left for improved readability.
    - Updated image class to use aspect ratio for better responsiveness and visual consistency.

--- components/blog/Header.vue
+++ components/blog/Header.vue
@@ -20,14 +20,14 @@ withDefaults(defineProps<Props>(), {
 
 <template>
   <header>
-    <h1 class="text-xl dark:text-zinc-300 md:text-3xl lg:text-4xl m-7 font-bold text-center">
+    <h1 class="text-xl dark:text-zinc-300 md:text-3xl lg:text-4xl my-7 font-bold text-left">
       {{ title || '' }}
     </h1>
     <NuxtImg
       :src="image || ''"
       :alt="alt || ''"
       width="600"
-      class="m-auto rounded-2xl shadow-lg h-32 md:h-72 w-4/6 md:w-4/5 content-center object-cover"
+      class="m-auto rounded-2xl shadow-lg aspect-video w-full content-center object-cover"
     />
     <p
       class="text-xs sm:text-sm my-3 max-w-xl mx-auto text-center text-zinc-600 dark:text-zinc-400"

616b168751985040ed23516c43b12ee646503ae2
Michael0520 <michael860520@gmail.com>
2025-01-13 22:09:22 +0800

    feat: enhance hero section with animated text components
    - Replaced static title and description with SparklesText and FlipWords components for dynamic text animations.
    - SparklesText adds animated sparkles around the title, while FlipWords cycles through key skills with fade-in effects.
    - Improved visual appeal and interactivity of the hero section, enhancing user engagement.

--- components/main/hero.vue
+++ components/main/hero.vue
@@ -1,23 +1,36 @@
 <script setup lang="ts">
 import { homePage } from '~/data'
+import FlipWords from '~/components/ui/FilpWords.vue'
+import SparklesText from '~/components/ui/SparklesText.vue'
 </script>
 
 <template>
   <div class="container mx-auto">
     <div class="grid grid-cols-1 sm:grid-cols-2 items-center">
-      <div class="px-6">
-        <h1
-          class="text-[#3F4AE8] dark:text-[#6871e9] font-semibold leading-tight text-4xl md:text-5xl my-5"
-        >
-          {{ homePage.title }}
-        </h1>
-        <p class="dark:text-zinc-300">
-          {{ homePage.description }}
-        </p>
+      <!-- hero text -->
+      <div class="flex h-[40rem] items-center justify-center px-4">
+        <div class="mx-auto text-4xl font-normal text-neutral-600 dark:text-neutral-400">
+
+            <SparklesText
+:text="homePage.title" :colors="{ first: '#9E7AFF', second: '#FE8BBB' }" :sparkles-count="10"
+              class="my-8" />
+
+          <p class="dark:text-zinc-300">
+            A Software Engineer. Get Web Development,
+
+            <FlipWords
+:words="['Typescript.', 'React/VueJs.', 'Next/Nuxt.']" :duration="3000"
+              class="text-4xl !text-primary" />
+            <br />
+            <br />
+            Related Articles, Tips, Learning resources and more.
+
+          </p>
+        </div>
       </div>
+
+      <!-- hero image -->
       <div class="p-6 justify-self-center">
-        <!-- <LogoDog /> -->
-        <!-- <img src="~/assets/images/logo.jpg" alt="dog" /> -->
         <NuxtImg src="/logo.jpg" width="450" height="500" quality="50" class="rounded-md" />
       </div>
     </div>
